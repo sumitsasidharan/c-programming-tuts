c73dea0e0b6f53787ceb4b61ef0eac5336ff32a8
sumitsasidharan <s.sumit196@gmail.com>
2023-02-18 20:33:04 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## How to Compile C programs using GCC compiler
+
+- `gcc filename.c`
+- `./a.exe`
---
+++ exercise1.c
@@ -0,0 +1,11 @@
+// Program to find
+
+#include <stdio.h>
+
+int main()
+{
+
+   printf("Hello World.. \n");
+
+   return 0;
+}
